fdf1e0afaa36026ad51263861c6b2cf485a523b8
Matheus Waked Franca <matheus363@hotmail.com>
2023-05-08 21:52:09 -0300

    Project created

---
+++ src/test/java/com/waked/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waked.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
